b13b4681fd4f0644852d802f5ae9a251f8fc9a93
Lang-Lovdog <brandon.mozart@gmail.com>
2025-08-03 19:26:53 -0600

    NewOrg Re-config

--- lua/config/neworg.lua
+++ lua/config/neworg.lua
@@ -3,19 +3,26 @@ local JournalPath = "$HOME/Documentos/Journal/"
 require("neorg").setup {
   load = {
     ["core.defaults"] = {},
+    ["core.keybinds"] = {},
     ["core.concealer"] = {},
+    ["core.autocommands"] = {},
+    ["core.neorgcmd"] = {},
     ["core.highlights"] = {},
-    ["core.latex.renderer"] = {
-      config={
-        conceal = true,
-        engine = "xelatex",
-        debounce_ms = 350,
-        dpi = 300,
-        renderer = "core.integrations.image",
-      },
-    },
-    ["core.integrations.nvim-cmp"] = {},
     ["core.completion"] = { config = { engine = "nvim-cmp" } },
+    ["core.latex.renderer"] = {},
+    --["core.latex.renderer"] = {
+    --  config={
+    --    conceal = true,
+    --    engine = "xelatex",
+    --    debounce_ms = 350,
+    --    dpi = 300,
+    --    renderer = "core.integrations.image",
+    --  },
+    --},
+    ["core.integrations.treesitter"] = {},
+    ["core.integrations.image"] = {},
+    ["core.integrations.nvim-cmp"] = {},
+    ["core.integrations.otter"] = {},
     ["core.integrations.treesitter"] = {
       config ={
         norg = {
@@ -53,8 +60,6 @@ end, {})
 
 -- Command to commit and push Journal files
 vim.api.nvim_create_user_command("JournalUpdate", function()
-  -- Get commit message
-  local message = vim.fn.input("Commit message: ")
   -- Commit and push
   print(vim.fn.system("cd " .. JournalPath .. " && git pull origin historia"))
 end, {})
