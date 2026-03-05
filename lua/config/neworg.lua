17c1b1a618d467fb670482547289654fb337c79a
Lang-Lovdog <brandon.mozart@gmail.com>
2026-03-05 16:27:07 -0600

    Norg journals y syntax acutalizado

--- lua/config/neworg.lua
+++ lua/config/neworg.lua
@@ -4,11 +4,8 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
   pattern = "*.norg",
   callback = function()
     vim.bo.filetype = "norg"
-    vim.treesitter.start()
+    -- vim.treesitter.start()
     -- This forces the legacy syntax engine to stay awake for the injections
-    -- vim.cmd([[syntax on]]) 
-    -- Manually load the liotree syntax into the current session
-    --vim.cmd([[runtime! syntax/liotree.vim]])
     -- Put colorcolumn at 64 and 128
     vim.opt.colorcolumn = "64,128"
     vim.opt.wrap = false
@@ -52,39 +49,18 @@ require("neorg").setup {
     ["core.highlights"] = {},
     ["core.completion"] = { config = { engine = "nvim-cmp" } },
     ["core.latex.renderer"] = {},
-    --["core.latex.renderer"] = {
-    --  config={
-    --    conceal = true,
-    --    engine = "xelatex",
-    --    debounce_ms = 350,
-    --    dpi = 300,
-    --    renderer = "core.integrations.image",
-    --  },
-    --},
---    ["core.integrations.treesitter"] = {},
+    ["core.integrations.treesitter"] = {},
     ["core.integrations.image"] = {},
     ["core.integrations.nvim-cmp"] = {},
     ["core.integrations.otter"] = {},
-    --["core.integrations.treesitter"] = {
-    --  config ={
-    --    norg = {
-    --      parser_configs ={
-    --        url="https://github.com/nvim-neorg/tree-sitter-norg",
-    --      },
-    --    },
-    --    norg_meta = {
-    --      parser_configs = {
-    --        url = "https://github.com/nvim-neorg/tree-sitter-norg-meta",
-    --      },
-    --    },
-    --  },
-    --},
     ["core.dirman"] = {
       config = {
         workspaces = {
           hataraki = JournalPath .. "HatarakiNotes",
           personal = JournalPath .. "PersonalJournal",
           MiMundoSciFi = JournalPath .. "MiMundoSciFi",
+          WulfusCanidae = JournalPath .. "WulfusCanidae",
+          LovoFriki = JournalPath .. "LovoFriki",
         },
       },
     },
@@ -92,17 +68,21 @@ require("neorg").setup {
 }
 
 -- Command to commit and push Journal files
-vim.api.nvim_create_user_command("JournalCommit", function()
+vim.api.nvim_create_user_command("JournalCommit", function(args)
   -- Get commit message
-  local message = vim.fn.input("Commit message: ")
+  local message = args.fargs[1]
+  -- For sourrounding quotes
+  if string.find(message, "\"") == nil then
+    message = "\"" .. message .. "\""
+  end
   -- Commit and push
-  print(vim.fn.system("cd " .. JournalPath .. " && git add . && git commit -m '" .. message .. "' && git push origin historia "))
+  vim.cmd("!(cd " .. JournalPath .. " && git add . && git commit -m '" .. message .. "' && git push origin historia)")
 end, {})
 
 -- Command to commit and push Journal files
 vim.api.nvim_create_user_command("JournalUpdate", function()
   -- Commit and push
-  print(vim.fn.system("cd " .. JournalPath .. " && git pull origin historia"))
+  vim.cmd("!(cd " .. JournalPath .. " && git pull origin historia)")
 end, {})
 
 
